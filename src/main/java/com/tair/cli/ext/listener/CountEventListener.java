2a6c655d4aec388cbe54bc62874f71d1175e0218
Baoyi Chen <chen.bao.yi@qq.com>
2022-02-21 10:31:43 +0800

    tair hackathon

--- src/main/java/com/tair/cli/ext/listener/CountEventListener.java
+++ src/main/java/com/tair/cli/ext/listener/CountEventListener.java
@@ -35,6 +35,13 @@ import com.tair.cli.util.OutputStreams;
 public class CountEventListener extends AbstractEventListener {
 	
 	private long count;
+	private long strCount;
+	private long setCount;
+	private long listCount;
+	private long hashCount;
+	private long zsetCount;
+	private long moduleCount;
+	private long streamCount;
 	private Configure configure;
 	private Escaper escaper = new JsonEscaper();
 	
@@ -49,11 +56,57 @@ public class CountEventListener extends AbstractEventListener {
 			setContext(dkv);
 			apply(dkv);
 		} else if (event instanceof PostRdbSyncEvent) {
-			escaper.encode(count, out);
-			OutputStreams.writeQuietly('\n', out);
+			json();
 			OutputStreams.flushQuietly(out);
 			OutputStreams.closeQuietly(out);
 		}
+	}
+	
+	protected void json() {
+		OutputStreams.write('{', out);
+		emitField("total", count);
+		if (strCount > 0) {
+			OutputStreams.write(',', out);
+			emitField("string", strCount);
+		}
+		if (setCount > 0) {
+			OutputStreams.write(',', out);
+			emitField("set", setCount);
+		}
+		if (listCount > 0) {
+			OutputStreams.write(',', out);
+			emitField("list", listCount);
+		}
+		if (hashCount > 0) {
+			OutputStreams.write(',', out);
+			emitField("hash", hashCount);
+		}
+		if (zsetCount > 0) {
+			OutputStreams.write(',', out);
+			emitField("zset", zsetCount);
+		}
+		if (moduleCount > 0) {
+			OutputStreams.write(',', out);
+			emitField("module", moduleCount);
+		}
+		if (streamCount > 0) {
+			OutputStreams.write(',', out);
+			emitField("stream", streamCount);
+		}
+		OutputStreams.write('}', out);
+		separator();
+	}
+	
+	private void emitField(String field, long value) {
+		emitString(field.getBytes());
+		OutputStreams.write(':', out);
+		escaper.encode(String.valueOf(value).getBytes(), out);
+	}
+	
+	private void emitString(byte[] str) {
+		OutputStreams.write('"', out);
+		escaper.encode(str, out);
+		OutputStreams.write('"', out);
 	}
 	
 	protected void separator() {
@@ -63,30 +116,35 @@ public class CountEventListener extends AbstractEventListener {
 	@Override
 	public  <T> T applyString(RedisInputStream in, int version) throws IOException {
 		count += 1;
+		strCount += 1;
 		return (T) getContext();
 	}
 	
 	@Override
 	public <T> T applyList(RedisInputStream in, int version) throws IOException {
 		count += 1;
+		listCount += 1;
 		return (T) getContext();
 	}
 	
 	@Override
 	public <T> T applySet(RedisInputStream in, int version) throws IOException {
 		count += 1;
+		setCount += 1;
 		return (T) getContext();
 	}
 	
 	@Override
 	public <T> T applyZSet(RedisInputStream in, int version) throws IOException {
 		count += 1;
+		zsetCount += 1;
 		return (T) getContext();
 	}
 	
 	@Override
 	public <T> T applyZSet2(RedisInputStream in, int version) throws IOException {
 		count += 1;
+		zsetCount += 1;
 		return (T) getContext();
 	}
 	
@@ -99,66 +157,77 @@ public class CountEventListener extends AbstractEventListener {
 	@Override
 	public <T> T applyHashZipMap(RedisInputStream in, int version) throws IOException {
 		count += 1;
+		hashCount += 1;
 		return (T) getContext();
 	}
 	
 	@Override
 	public <T> T applyListZipList(RedisInputStream in, int version) throws IOException {
 		count += 1;
+		listCount += 1;
 		return (T) getContext();
 	}
 	
 	@Override
 	public <T> T applySetIntSet(RedisInputStream in, int version) throws IOException {
 		count += 1;
+		setCount += 1;
 		return (T) getContext();
 	}
 	
 	@Override
 	public <T> T applyZSetZipList(RedisInputStream in, int version) throws IOException {
 		count += 1;
+		zsetCount += 1;
 		return (T) getContext();
 	}
 	
 	@Override
 	public <T> T applyZSetListPack(RedisInputStream in, int version) throws IOException {
 		count += 1;
+		zsetCount += 1;
 		return (T) getContext();
 	}
 	
 	@Override
 	public <T> T applyHashZipList(RedisInputStream in, int version) throws IOException {
 		count += 1;
+		hashCount += 1;
 		return (T) getContext();
 	}
 	
 	@Override
 	public <T> T applyHashListPack(RedisInputStream in, int version) throws IOException {
 		count += 1;
+		hashCount += 1;
 		return (T) getContext();
 	}
 	
 	@Override
 	public <T> T applyListQuickList(RedisInputStream in, int version) throws IOException {
 		count += 1;
+		listCount += 1;
 		return (T) getContext();
 	}
 	
 	@Override
 	public <T> T applyListQuickList2(RedisInputStream in, int version) throws IOException {
 		count += 1;
+		listCount += 1;
 		return (T) getContext();
 	}
 	
 	@Override
 	public <T> T applyModule2(RedisInputStream in, int version) throws IOException {
 		count += 1;
+		moduleCount += 1;
 		return (T) getContext();
 	}
 	
 	@Override
 	public <T> T applyStreamListPacks(RedisInputStream in, int version) throws IOException {
 		count += 1;
+		streamCount += 1;
 		return (T) getContext();
 	}
 }
